0f184f1d84fea24a714c52147f65d6cbd105dec2
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-26 18:32:08 +1300

    Defined sample() to find the iteration count of numbers within a given space

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -18,7 +18,7 @@ int iterate(const std::complex<double> &init, const std::complex<double> &c, con
   static int count=0;
   int itr;
   std::complex<double> x=init;
-  
+
   for (itr=0;itr<max_itr;++itr)
   {
     if (abs(x)>2)
@@ -29,4 +29,23 @@ int iterate(const std::complex<double> &init, const std::complex<double> &c, con
     x=x*x+c;
   }
   return itr+1;
+}
+
+void __declspec(dllexport) sample(int **iterations, const double x, const double y, const int max_itr, const int xresolution, 
+  const int yresolution, const double startx, const double endx, const double starty, const double endy)
+{
+  const double deltax=(endx-startx)/xresolution,deltay=(endy-starty)/yresolution;
+  const int total=xresolution*yresolution;
+
+  std::complex<double> init(x,y);
+
+  for (int itr=0;itr<xresolution;++itr)
+  {
+    double real=startx+deltax*itr;
+    for (int jtr=0;jtr<yresolution;++jtr)
+    {
+      double imag=starty+deltay*jtr;
+      *(*(iterations+itr)+jtr)=iterate(init,std::complex<double>(real,imag),max_itr);
+    }
+  }
 }
