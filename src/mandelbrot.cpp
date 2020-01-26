846aa46fd8ca13e3425562026dbf6e4081a93700
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-26 17:57:19 +1300

    Defined iterate(), to iterate and determine if a given number is in the mandelbrot set

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -10,3 +10,23 @@
 
 
 #include <fractal.hpp>
+#include <iostream>
+#include <chrono>
+
+int iterate(const std::complex<double> &init, const std::complex<double> &c, const int max_itr)
+{
+  static int count=0;
+  int itr;
+  std::complex<double> x=init;
+  
+  for (itr=0;itr<max_itr;++itr)
+  {
+    if (abs(x)>2)
+    {
+      if (itr>count) { count=itr; }
+      break;
+    }
+    x=x*x+c;
+  }
+  return itr+1;
+}
