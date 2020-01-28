7bbdcb810e4b86e1ca9076d931b4d9d05aba2d5e
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-28 21:24:54 +1300

    Started definition of plot_mandelbrot(); fixed some rather egregious errors in sample_mandelbrot()

--- fractal.py
+++ fractal.py
@@ -11,6 +11,7 @@
 
 import ctypes as ct
 import numpy as np
+import matplotlib.pyplot as plt
 
 from huygens.interf import c_matrix
 
@@ -23,13 +24,25 @@ _libc=ct.cdll.LoadLibrary('./bin/fractal.dll')
 _sample_mandelbrot=getattr(_libc,'?sample_mandelbrot@@YAXPEAPEAHHHHNNNN@Z')
 
 # assign arg and return types
-_sample_mandelbrot.argtypes=[ct.POINTER(ct.POINTER(ct.c_int)),ct.c_int,ct.c_int,ct.c_double,ct.c_double,ct.c_double,ct.c_double]
+_sample_mandelbrot.argtypes=[ct.POINTER(ct.POINTER(ct.c_int)),ct.c_int,ct.c_int,ct.c_int,ct.c_double,ct.c_double,ct.c_double,ct.c_double]
 _sample_mandelbrot.restype=None
+
+def plot_mandelbrot(iterations,file_name='out.png'):
+  fig,ax=plt.subplots()
+  fig.subplots_adjust(0,0,1,1)
+
+  ax.set_xticks([])
+  ax.set_yticks([])
+  ax.set_xticklabels([])
+  ax.set_yticklabels([])
+
+  ax.imshow(iterations,cmap='Spectral_r')
+  plt.savefig(file_name)
 
 def sample_mandelbrot(central_point,x_span,y_span,x_resolution,y_resolution,max_itr):
   startx=central_point[0]-x_span/2.
-  starty=central_point[0]-x_span/2.
-  endx=central_point[1]+y_span/2.
+  starty=central_point[1]-y_span/2.
+  endx=central_point[0]+x_span/2.
   endy=central_point[1]+y_span/2.
   
   tmp,act=c_matrix(ct.c_int,y_resolution,x_resolution)
@@ -40,4 +53,3 @@ def sample_mandelbrot(central_point,x_span,y_span,x_resolution,y_resolution,max_
 
   del tmp
   return np.ctypeslib.as_array(act)
-  
