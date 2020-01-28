aafeab974d595b6ce7dd764b3762980af037850a
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-28 21:41:43 +1300

    Started definition of produce_mandelbrot_visualisation(), showing the template it will take

--- main.py
+++ main.py
@@ -9,5 +9,23 @@
 # Main file to produce visualisations of fractals
 
 
+import errno
+import os
+
+from fractal import *
+
+def produce_mandelbrot_visualisation(example='one',show_fig=False,save_fig=True,file_dir='./out/',file_name='out.pdf'):
+
+  try:
+    os.makedirs(file_dir) 
+  except OSError as err:
+    if err.errno != errno.EEXIST:
+      raise
+
+  if example=='one':
+    iterations=sample_mandelbrot((-0.7435669,-0.1314023),0.0022878,0.0022878,5001,5001,1000)
+    plot_mandelbrot(iterations,file_name=file_dir+file_name)
+    return
+
 if __name__=='__main__':
-  pass
+  produce_mandelbrot_visualisation()
