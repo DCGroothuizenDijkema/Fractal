103347f59be07a54f0ccb5282bcef7e9309fa874
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-31 22:01:47 +1300

    Refactored produce_mandelbrot_visualisation() to match new helper functions

--- main.py
+++ main.py
@@ -9,39 +9,33 @@
 # Main file to produce visualisations of fractals
 
 
-import errno
-import os
+import matplotlib.cm as cm
 
 from fractal import *
 
-def produce_mandelbrot_visualisation(example='zoom_level_zero',resolution=10001,show_fig=False,save_fig=True,file_dir='./out/'
-  ,file_name='out.pdf'):
-
-  try:
-    os.makedirs(file_dir) 
-  except OSError as err:
-    if err.errno != errno.EEXIST:
-      raise
+def produce_mandelbrot_visualisation(example='zoom_level_zero',fractal_resolution=3001,show_fig=False,save_fig=True,
+  file_name='mandelbrot.pdf',dpi=1200,verbose=False):
 
   dx=3
   dy=2
 
   if example=='zoom_level_zero':
     span=1
-    iterations,limit=sample_mandelbrot((-0.7,0.0),dx*span,dy*span,dx*resolution,dy*resolution,1000)
-    plot_mandelbrot(iterations,limit,file_name=file_dir+file_name,fig_inches=(6*dx,6*dy))
+    iterations,limit=sample_mandelbrot((-0.7,0.0),dx*span,dy*span,dx*fractal_resolution,dy*fractal_resolution,1000,verbose=verbose)
+    plot_mandelbrot(iterations,limit,file_name=file_name,fig_inches=(6*dx,6*dy),dpi=dpi)
   elif example=='zoom_level_one':
     span=0.007
-    iterations,limit=sample_mandelbrot((-0.744030,0.1263),dx*span,dy*span,dx*resolution,dy*resolution,1000)
-    plot_mandelbrot(iterations,limit,file_name=file_dir+file_name,fig_inches=(6*dx,6*dy),dpi=300)
+    iterations,limit=sample_mandelbrot((-0.744030,0.1263),dx*span,dy*span,dx*fractal_resolution,dy*fractal_resolution,1000,verbose=verbose)
+    plot_mandelbrot(iterations,limit,file_name=file_name,fig_inches=(6*dx,6*dy),dpi=dpi)
   elif example=='zoom_level_two':
     span=0.0065
-    iterations,limit=sample_mandelbrot((-0.775,0.121),dx*span,dy*span,dx*resolution,dy*resolution,1000)
-    plot_mandelbrot(iterations,limit,file_name=file_dir+file_name,fig_inches=(6*dx,6*dy),dpi=300)
+    iterations,limit=sample_mandelbrot((-0.775,0.121),dx*span,dy*span,dx*fractal_resolution,dy*fractal_resolution,1000,verbose=verbose)
+    plot_mandelbrot(iterations,limit,file_name=file_name,fig_inches=(6*dx,6*dy),dpi=dpi)
   elif example=='zoom_level_three':
     span=0.0011439
-    iterations,limit=sample_mandelbrot((-0.7439668,0.1314023),dx*span,dy*span,dx*resolution,dy*resolution,1000)
-    plot_mandelbrot(iterations,limit,file_name=file_dir+file_name,fig_inches=(6*dx,6*dy),dpi=1200)
+    iterations,limit=sample_mandelbrot((-0.7439668,0.1314023),dx*span,dy*span,dx*fractal_resolution,dy*fractal_resolution,1000
+      ,verbose=verbose)
+    plot_mandelbrot(iterations,limit,file_name=file_name,fig_inches=(6*dx,6*dy),dpi=dpi)
 
 if __name__=='__main__':
-  produce_mandelbrot_visualisation('zoom_level_three')
+  produce_mandelbrot_visualisation()
