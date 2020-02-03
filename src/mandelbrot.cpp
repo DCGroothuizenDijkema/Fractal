b33a386ac56d78d3d0cf8564c6b8c5e80f15ef91
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-02-03 18:35:02 +1300

    Tidy up and formatting

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -11,25 +11,20 @@
 
 #include <fractal.hpp>
 
-int iterate(const std::complex<double> &init, const std::complex<double> &c, const int max_itr)
+int iterate(std::complex<double> x, const std::complex<double> &c, const int max_itr)
 {
-  static int count=0;
   int itr;
-  std::complex<double> x=init;
 
   for (itr=0;itr<max_itr;++itr)
   {
-    if (abs(x)>2)
-    {
-      return ++itr;
-    }
+    if (abs(x)>2) { return ++itr; }
     x=x*x+c;
   }
   return std::numeric_limits<int>::max();
 }
 
-void __declspec(dllexport) sample_mandelbrot(int **iterations, const int max_itr, const int xresolution, const int yresolution, int * const limit,
-  const double startx, const double endx, const double starty, const double endy, const bool verbose)
+void __declspec(dllexport) sample_mandelbrot(int **iterations, const int max_itr, const int xresolution, const int yresolution,
+  int * const limit, const double startx, const double endx, const double starty, const double endy, const bool verbose)
 {
   const double deltax=(endx-startx)/xresolution,deltay=(endy-starty)/yresolution;
   const int total=xresolution*yresolution;
