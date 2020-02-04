9e80267d31f8e2328bb3d8ed63950642852f5ac7
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-02-04 18:47:01 +1300

    Created master files for testing

---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+//
+//                                                                                                                                       //
+// test.cpp                                                                                                                              //
+//                                                                                                                                       //
+// D. C. Groothuizen Dijkema - January, 2020                                                                                             //
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+//
+
+// Test file for Newton's fractals
+
+
+// because Microsoft is the worst
+#define NOMINMAX
+
+#define BOOST_TEST_MODULE NEWTONTESTING
+#include <boost/test/included/unit_test.hpp>
+
+#include <test-newton.hpp>
