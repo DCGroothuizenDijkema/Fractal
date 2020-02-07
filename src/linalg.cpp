24c97b7eea76e4b13f976b1573c3b487739feec1
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-02-08 12:47:47 +1300

    Fixed out-of-bounds indexing issue

--- src/linalg.cpp
+++ src/linalg.cpp
@@ -13,22 +13,20 @@
 
 double **initialise_companion_matrix(const int degree)
 {
+  if (degree<2) { throw std::invalid_argument("`degree` must be greater than or equal to 2"); }
   double **mat=new double*[degree];
   for (int itr=0;itr<degree;++itr)
   {
     *(mat+itr)=new double[degree];
     std::fill(*(mat+itr),*(mat+itr)+degree,0.);
   }
-  for (int itr=1;itr<degree;++itr)
-  {
-    for (int jtr=0;jtr<degree-1;++jtr) { *(*(mat+itr)+jtr)=1.; }
-  }
+  for (int itr=1,jtr=0;itr<degree,jtr<degree-1;++itr,++jtr) { *(*(mat+itr)+jtr)=1.; }
   return mat;
 }
 
 void assign_companion_matrix(double * const * const mat, double const * const coeffs, const int degree)
 {
-  for (int itr=0;itr<degree;++itr) { *(*(mat+itr)+degree)=*(coeffs+itr); }
+  for (int itr=0;itr<degree;++itr) { *(*(mat+itr)+degree-1)=*(coeffs+itr); }
 }
 
 void __declspec(dllexport) roots(double const * const coeffs, double * const roots_re, double * const roots_im, const int degree)
