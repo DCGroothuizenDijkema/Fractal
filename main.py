16eb511eb37d6f401b9a9d80271f17b8cb551231
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-03-05 13:39:35 +1300

    Moved redundant plot_mandelbrot()

--- main.py
+++ main.py
@@ -20,20 +20,18 @@ def produce_mandelbrot_visualisation(example='zoom_level_zero',fractal_resolutio
   if example=='zoom_level_zero':
     span=1
     iterations,limit=sample_mandelbrot((-0.7,0.0),dx*span,dy*span,dx*fractal_resolution,dy*fractal_resolution,1000,verbose=verbose)
-    plot_mandelbrot(iterations,limit,file_name=file_name,fig_inches=(6*dx,6*dy),dpi=dpi)
   elif example=='zoom_level_one':
     span=0.007
     iterations,limit=sample_mandelbrot((-0.744030,0.1263),dx*span,dy*span,dx*fractal_resolution,dy*fractal_resolution,1000,verbose=verbose)
-    plot_mandelbrot(iterations,limit,file_name=file_name,fig_inches=(6*dx,6*dy),dpi=dpi)
   elif example=='zoom_level_two':
     span=0.0065
     iterations,limit=sample_mandelbrot((-0.775,0.121),dx*span,dy*span,dx*fractal_resolution,dy*fractal_resolution,1000,verbose=verbose)
-    plot_mandelbrot(iterations,limit,file_name=file_name,fig_inches=(6*dx,6*dy),dpi=dpi)
   elif example=='zoom_level_three':
     span=0.0011439
     iterations,limit=sample_mandelbrot((-0.7439668,0.1314023),dx*span,dy*span,dx*fractal_resolution,dy*fractal_resolution,1000
       ,verbose=verbose)
-    plot_mandelbrot(iterations,limit,file_name=file_name,fig_inches=(6*dx,6*dy),dpi=dpi)
+  
+  plot_mandelbrot(iterations,limit,file_name=file_name,fig_inches=(6*dx,6*dy),dpi=dpi)
 
 if __name__=='__main__':
   produce_mandelbrot_visualisation()
