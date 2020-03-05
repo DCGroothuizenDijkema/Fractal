e1404cb3f050dc584ebc4d29fad20a452320117d
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-03-05 13:54:15 +1300

    Defined sample_newton() Python function to interface with library functions

--- fractal.py
+++ fractal.py
@@ -93,4 +93,38 @@ def plot_newton(roots,iterations,limit,colors,log=True,show_fig=False,save_fig=T
   pass
 
 def sample_newton(coeffs,central_point,x_span,y_span,x_resolution,y_resolution,max_itr,num_threads=1,verbose=False):
-  pass
+  startx=central_point[0]-x_span/2.
+  starty=central_point[1]-y_span/2.
+  endx=central_point[0]+x_span/2.
+  endy=central_point[1]+y_span/2.
+  limit=c_pointer(ct.c_int,0)
+  
+  tmp_re,act_re=c_matrix(ct.c_double,y_resolution,x_resolution)
+  tmp_im,act_im=c_matrix(ct.c_double,y_resolution,x_resolution)
+  tmp_itr,act_itr=c_matrix(ct.c_int,y_resolution,x_resolution)
+
+  poly_coeffs=c_vector(ct.c_double,len(coeffs),coeffs)
+  poly_degree=len(coeffs)-1
+
+  _sample_newton(
+    tmp_re,tmp_im,tmp_itr,poly_coeffs,ct.c_int(max_itr),ct.c_int(num_threads),ct.c_int(poly_degree)
+    ,ct.c_int(x_resolution),ct.c_int(y_resolution),limit,ct.c_double(startx),ct.c_double(endx)
+    ,ct.c_double(starty),ct.c_double(endy),ct.c_bool(verbose)
+  )
+
+  del tmp_itr
+
+  roots=1j*np.ctypeslib.as_array(act_im)
+  roots+=np.ctypeslib.as_array(act_re)
+
+  actuals=np.roots(coeffs)
+  tmp_ind,act_ind=c_matrix(ct.c_int,y_resolution,x_resolution)
+  roots_re=c_vector(ct.c_double,len(actuals),np.real(actuals))
+  roots_im=c_vector(ct.c_double,len(actuals),np.imag(actuals))
+
+  _assign_roots(tmp_ind,tmp_re,tmp_im,roots_re,roots_im,ct.c_int(poly_degree),ct.c_int(x_resolution),ct.c_int(y_resolution))
+
+  del tmp_re
+  del tmp_im
+
+  return np.flipud(roots),np.flipud(np.ctypeslib.as_array(act_ind)),np.flipud(np.ctypeslib.as_array(act_itr)),limit.contents.value
