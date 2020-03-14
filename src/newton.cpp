98885fc134cb4a1a9296adb7edc5de51e4f269d1
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-03-14 20:44:59 +1300

    Added documentation

--- src/newton.cpp
+++ src/newton.cpp
@@ -227,6 +227,32 @@ int __declspec(dllexport) sample_newton(double **re, double **im, int **iteratio
 void __declspec(dllexport) assign_roots(int * const * const index, const double * const * const re, const double * const * const im
   , const double * const roots_re, const double * const roots_im, const int degree, const int xresolution, const int yresolution)
 {
+  //
+  // Determine the roots of a polynomial the numbers in a given subset of the complex plane converge to with Newton's method
+  //
+  // parameters
+  // ----------
+  // re,im : double **
+  //  - 2D arrays to write out the root which the numbers in the subset cnoverged to
+  // iterations : int **
+  //  - 2D array to write out either the number of iterations needed reach a root or a marker that this root could not be reached
+  // coeffs : double *
+  //  - the coefficients of the polynomial given in order of the lowest degree to highest
+  //    see polynomial_and_deriv() for requirements
+  // max_itr : const int
+  //  - the maximum number of iterations allowed
+  // degree : const int
+  //  - the degree of the polynomial
+  // num_threads : const int
+  //  - the number of threads to use in computation
+  // xresolution,yresolution : const int 
+  //  - the number of steps to take in the x- and y-direction (the real and imaginary components)
+  // startx,endx,starty,endy : const int
+  //  - the first and last values to sample at 
+  // verbose : bool
+  //  - flag to control logging to console
+  //
+
   // get a list of all roots, formed from the input vectors giving the real and imaginary components of the roots
   std::vector<std::complex<double>> roots;
   zip(roots_re,roots_re+degree,roots_im,roots_im+degree,std::back_inserter(roots));
