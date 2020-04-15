1705e9c86d10369f91b7fed8b4945ef46c9ce8c5
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-04-16 10:38:57 +1200

    Created header file for CUDA implementation

---
+++ src/cu_fractal.hpp
@@ -0,0 +1,20 @@
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+//
+//                                                                                                                                       //
+// cu_fractal.hpp                                                                                                                        //
+//                                                                                                                                       //
+// D. C. Groothuizen Dijkema - April, 2020                                                                                               //
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+//
+
+// Header file for the CUDA accelerated implementation of the Fractal project
+
+
+#pragma once
+
+#ifndef CUFRACTAL_H__
+#define CUFRACTAL_H__
+
+#include <cuComplex.h>
+#include <thrust/pair.h>
+
+#endif // !CUFRACTAL_H__
