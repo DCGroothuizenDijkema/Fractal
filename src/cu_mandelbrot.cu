3d0b6d8acb30d91a94ed5e605f0a5d1286761131
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-04-17 14:26:07 +1200

    Defined device function iterate() to iterate a point

--- src/cu_mandelbrot.cu
+++ src/cu_mandelbrot.cu
@@ -11,7 +11,15 @@
 
 #include <cu_fractal.hpp>
 
-__device__ int iterate(cuDoubleComplex x, const cuDoubleComplex &c, const int max_itr);
+__device__ int iterate(cuDoubleComplex x, const cuDoubleComplex &c, const int max_itr)
+{
+  for (int itr=0;itr<max_itr;++itr)
+  {
+    if (cuCabs(x)>2) { return itr; }
+    x=cuCadd(cuCmul(x,x),c);
+  }
+  return NPP_MAX_32S;
+}
 
 __global__ void compute_mandelbrot(int * const d_iterations, const int max_itr, const int xresolution, const int yresolution
   , const double startx, const double starty, const double deltax, const double deltay, const int total, bool verbose);
