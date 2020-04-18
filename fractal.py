caa40f5eb7a49d26f9bd8656b08b08b98899ae89
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-04-18 22:34:55 +1200

    Created Python interface with sample_julia()

--- fractal.py
+++ fractal.py
@@ -49,14 +49,19 @@ if CUDA_ENABLED:
 
   # extract the functions
   _sample_mandelbrot_cuda=getattr(_libc_cuda,'?sample_mandelbrot@@YAHQEAHHHHNNNN_N@Z')
+  _sample_julia_cuda=getattr(_libc_cuda,'?sample_julia@@YAHQEAHNNHHHNNNN_N@Z')
   _sample_newton_cuda=getattr(_libc_cuda,'?sample_newton@@YAHQEAN0QEAHQEBNHHHHNNNN_N@Z')
   _assign_roots_cuda=getattr(_libc_cuda,'?assign_roots@@YAXQEAHQEBN111HHH@Z')
 
   # assign arg and return types
-  _sample_mandelbrot_cuda.argtypes=[ct.POINTER(ct.c_int),ct.c_int,ct.c_int,ct.c_int,ct.c_double,ct.c_double,ct.c_double,ct.c_double,ct.c_bool]
+  _sample_mandelbrot_cuda.argtypes=[ct.POINTER(ct.c_int),ct.c_int,ct.c_int,ct.c_int,ct.c_double,ct.c_double,ct.c_double,ct.c_double
+    ,ct.c_bool]
   _sample_mandelbrot_cuda.restype=ct.c_int
-  _sample_newton_cuda.argtypes=[ct.POINTER(ct.c_double),ct.POINTER(ct.c_double),ct.POINTER(ct.c_int),ct.POINTER(ct.c_double),ct.c_int,ct.c_int
-    ,ct.c_int,ct.c_int,ct.c_double,ct.c_double,ct.c_double,ct.c_double,ct.c_bool]
+  _sample_julia_cuda.argtypes=[ct.POINTER(ct.c_int),ct.c_double,ct.c_double,ct.c_int,ct.c_int,ct.c_int,ct.c_double,ct.c_double,ct.c_double
+    ,ct.c_double,ct.c_bool]
+  _sample_julia_cuda.restype=ct.c_int
+  _sample_newton_cuda.argtypes=[ct.POINTER(ct.c_double),ct.POINTER(ct.c_double),ct.POINTER(ct.c_int),ct.POINTER(ct.c_double),ct.c_int
+    ,ct.c_int,ct.c_int,ct.c_int,ct.c_double,ct.c_double,ct.c_double,ct.c_double,ct.c_bool]
   _sample_newton_cuda.restype=ct.c_int
   _assign_roots_cuda.argtypes=[ct.POINTER(ct.c_int),ct.POINTER(ct.c_double),ct.POINTER(ct.c_double),ct.POINTER(ct.c_double)
     ,ct.POINTER(ct.c_double),ct.c_int,ct.c_int,ct.c_int]
@@ -162,7 +167,7 @@ def sample_mandelbrot(central_point,x_span,y_span,x_resolution,y_resolution,max_
 
 def sample_mandelbrot_cuda(central_point,x_span,y_span,x_resolution,y_resolution,max_itr,verbose=False):
   '''
-  Produce a sample of the Mandelbrot Set.
+  Produce a sample of the Mandelbrot Set with CUDA acceleration.
   
   Parameters
   ----------
@@ -202,6 +207,52 @@ def sample_mandelbrot_cuda(central_point,x_span,y_span,x_resolution,y_resolution
 
   # reshape into a 2D array and flip the rows because [startx,stary] is stored in [0,0]
   return np.flipud(np.reshape(np.ctypeslib.as_array(itr),(y_resolution,x_resolution))),limit
+
+def sample_julia_cuda(c,central_point,x_span,y_span,x_resolution,y_resolution,max_itr,verbose=False):
+  '''
+  Produce a sample of the Julia set of a given complex number with CUDA acceleration.
+  
+  Parameters
+  ----------
+  c : complex
+    - The complex number to find the julia set of.
+  central_point : 1D array-like
+    - The centre of the area to visualise.
+  x_span,y_span : int
+    - The span across each axis, with half of the span on either side of the centre.
+  x_resolution,y_resolution : int
+    - The number of pizels to divide the x- and y-axes into.
+  max_itr : int
+    - The number of iterations to compute before considering a point to not converge.
+  verbose : bool, optional.
+    - For verbose output.
+
+  Returns
+  -------
+  itr : 2D numpy.ndarray
+    - The number of iterations for a pixel exceed the bound.
+  limit : int
+    - The value which represents the bound was not exceeded.
+
+  '''
+  if not CUDA_ENABLED: raise CUDAWarning('CUDA library has not been implemented.')
+  # input setup
+  startx=central_point[0]-x_span/2.
+  starty=central_point[1]-y_span/2.
+  endx=central_point[0]+x_span/2.
+  endy=central_point[1]+y_span/2.
+  # array to store the iteration count for each pixel
+  itr=c_vector(ct.c_int,y_resolution*x_resolution)
+
+  # call the library function
+  limit=_sample_julia_cuda(
+    itr,ct.c_double(c.real),ct.c_double(c.imag),ct.c_int(max_itr),ct.c_int(x_resolution),ct.c_int(y_resolution)
+    ,ct.c_double(startx),ct.c_double(endx),ct.c_double(starty),ct.c_double(endy),ct.c_bool(verbose)
+  )
+
+  # reshape into a 2D array and flip the rows because [startx,stary] is stored in [0,0]
+  return np.flipud(np.reshape(np.ctypeslib.as_array(itr),(y_resolution,x_resolution))),limit
+
 
 def plot_newton_roots(roots,show_fig=False,save_fig=True,file_name='newtons_fractal_roots.pdf',fig_inches=(12,12),dpi=1200
   ,color_map=None):
